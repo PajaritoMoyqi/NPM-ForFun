64052c59223f7e6eb671452591899136d9ee827b
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2022-12-17 20:41:08 +0900

    get random color code method added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 export const getRandomNum = (initialNum) => {
   return Math.floor(Math.random()*(initialNum));
 } // get : 0 ~ less then initialNum
+
 export const getRandomNumNotSame = (initialNum, currentNum) => {
   let candidate = currentNum;
 
@@ -12,13 +13,20 @@ export const getRandomNumNotSame = (initialNum, currentNum) => {
 
   return candidate;
 } // get 0 ~ less then initialNum except currentNum
+
 export const getRandomIdx = (arr) => {
   return Math.floor(Math.random()*(arr.length));
 }
+
 export const getRandomItem = (arr) => {
   const randomIdx = Math.floor(Math.random()*(arr.length));
   return arr[randomIdx];
 }
+
+export const getRandomColorCode = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 
 
 // getArray
@@ -32,9 +40,11 @@ export const getArrFromNumToNum = (start, end) => {
 
   return initialArr;
 } // get : [start, start + 1, start + 2, ... , end -2, end -1, end]
+
 export const shuffleArray = (array) => {
   return array.sort(() => Math.random() - 0.5);
 }
+
 export const getShuffledArrFromNumToNum = (start, end) => {
   let initialArr = [];
 
@@ -44,6 +54,8 @@ export const getShuffledArrFromNumToNum = (start, end) => {
 
   return shuffleArray(initialArr);
 } // get : [start, start + 1, start + 2, ... , end -2, end -1, end]
+
+
 
 // string
 
